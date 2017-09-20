cd98b13e31998aed67fd7f06a116c32f162e32e0
MirelleVignal <mireille-vignal@orange.fr>
2017-09-20 13:12:18 +0200

    Correction des erreurs de syntaxe du programme

--- Programme-FT.py
+++ Programme-FT.py
@@ -1,16 +1,16 @@
 Personnages = ["Natsu Dragneel","Happy","Elfman","Frosh","Sheria","Juvia"]
 
-Citations = ["Je m'enflamme !","Aye !","C'est un hommes !","Fro pense la même chose.","C'est l'amour.","Grey-sama~"]
+Citations = ["Je m'enflamme !","Aye !","C'est un homme !","Fro pense la même chose.","C'est l'amour.","Grey-sama~"]
 
 import random
 
 def random_item(liste):
-    rand_numb=random.randint(0,len(listes)-1)
-    item=liste[random.randint]
+    rand_numb=random.randint(0,len(liste)-1)
+    item=liste[rand_numb]
     return item
 
 def message(personnage,citation):
-    return "{} a dit ; {}".format(personnage,citation)
+    return "{} a dit : {}".format(personnage,citation)
 
 user_answer=input("Tapez entrée si vous voulez une autre citation, ou X pour quitter le programme.")
 
